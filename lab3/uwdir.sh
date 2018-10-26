bdd24eda1f6fc718381fd9ae16267f9bf1112c0a
BasicEC <zak.vitaly@ya.ru>
2018-10-26 21:14:28 +0300

    lab3-1.0

---
+++ lab3/uwdir.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# var 4
+
+if [[ $# -eq 0 ]] 
+then echo "err: no args" >&2
+exit 1
+fi
+dir_info=$(ls -ld "$1" | awk '{print $1":"$3}')
+perm=${dir_info%:*}
+owner=${dir_info#*:}
+uid_gid=$(getent passwd | grep "$owner" | awk -F: '{print $3":"$4}')
+
+regex="[^:]+:[^:]+:[^:]+:${uid_gid#*:}:*"
+
+if [[ -n $(echo $perm | egrep "d....w....") ]]  
+then
+getent passwd | egrep -v "^$owner" | egrep "$regex" | cut -d: -f 1  
+fi
+if [[ -n $(echo $perm | egrep "d.w.......") ]]
+then echo $owner 
+fi
+if [[ -n $(echo $perm | egrep "d.......w.") ]] 
+then getent passwd | egrep -v "$regex" | cut -d: -f 1 
+fi
