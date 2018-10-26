d1380c3e3703e47d22188d62fbdb50f4ca77bde6
BasicEC <zak.vitaly@ya.ru>
2018-10-26 22:00:09 +0300

    err: this is not dir

--- lab3/uwdir.sh
+++ lab3/uwdir.sh
@@ -5,6 +5,10 @@ if [[ $# -eq 0 ]]
 then echo "err: no args" >&2
 exit 1
 fi
+if [[ !( -d $1 ) ]] 
+then echo "err: $1 isn't dir" >&2
+exit 1
+fi
 dir_info=$(ls -ld "$1" | awk '{print $1":"$3}')
 perm=${dir_info%:*}
 owner=${dir_info#*:}
