5a4d05d8ddc8006541cd5971f61df70448e4cd73
BasicEC <zak.vitaly@ya.ru>
2018-10-26 22:03:03 +0300

    \n

--- lab3/uwdir.sh
+++ lab3/uwdir.sh
@@ -5,10 +5,12 @@ if [[ $# -eq 0 ]]
 then echo "err: no args" >&2
 exit 1
 fi
+
 if [[ !( -d $1 ) ]] 
 then echo "err: $1 isn't dir" >&2
 exit 1
 fi
+
 dir_info=$(ls -ld "$1" | awk '{print $1":"$3}')
 perm=${dir_info%:*}
 owner=${dir_info#*:}
@@ -20,9 +22,11 @@ if [[ -n $(echo $perm | egrep "d....w....") ]]
 then
 getent passwd | egrep -v "^$owner" | egrep "$regex" | cut -d: -f 1  
 fi
+
 if [[ -n $(echo $perm | egrep "d.w.......") ]]
 then echo $owner 
 fi
+
 if [[ -n $(echo $perm | egrep "d.......w.") ]] 
 then getent passwd | egrep -v "$regex" | cut -d: -f 1 
 fi
